52120f1e270153555446b25bab8e9ef49c99657f
scottwmaxwell <scottwillardmaxwell@protonmail.com>
2023-10-30 18:52:54 -0700

    Added main Java Maven SpringBoot Project Files

---
+++ blogsite/src/test/java/com/group2/blogsite/BlogsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group2.blogsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
